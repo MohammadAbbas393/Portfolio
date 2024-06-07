39d74ab458698fbc1ab8aa0759b44a3b09b3b77f
MohammadAbbas393 <abbasgithub393@gmail.com>
2024-06-07 11:09:58 -0400

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import cleaner from "src/components/Projects/Cleaner.png";
+import cleaner from ".../Cleaner.png";
 
 function Projects() {
   return (
