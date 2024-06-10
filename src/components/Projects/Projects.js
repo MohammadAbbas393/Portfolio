083b83924568e09d3f785b472474aeb83efce962
MohammadAbbas393 <abbasgithub393@gmail.com>
2024-06-10 10:42:18 -0400

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -9,7 +9,7 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          My Recent <strong className="purple">Works</strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
@@ -17,7 +17,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={require("src/components/Projects/Cleaner.png")}
+              imgPath={require("../../Assets/Projects/Cleaner.png")}
               isBlog={false}
               title="Downloads Cleaner"
               description="Code that can organize your downloads folder instead of having them shuffled, they will go to multiple folders (Text, Audio, Pics, Programming, and Uncategorized). Under each folder, you have subfolders like in text you have (PDF, WORD) in Programming you have (Python, Java)."
@@ -26,7 +26,7 @@ function Projects() {
           </Col>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={require("src/Assets/Projects/Electricbg")}
+              imgPath={require("../../Assets/Projects/Electricbg.png")} // Ensure this path is also correct
               isBlog={false}
               title="Electric Background"
               description="This idea of the code was actually taken from CodePen but I developed the code by myself. The idea of doing that is I had a project to build an ITS website for Middlebury College especially the Digital Learning and Inquiry department so I needed some tech in the header."
