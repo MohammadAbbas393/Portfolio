486ac497f6885eb85a31c915cca7d1a26457c792
Mohammad Abbas <abbasgithub393@gmail.com>
2024-08-19 16:18:00 -0400

    Update ResumeNew.js

--- src/components/ResumeNew.js
+++ src/components/ResumeNew.js
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from './Particle';
-import pdf from '../Resume2024.pdf'; 
+import pdf from '../Resume2024.pdf';  // Ensure this path is correct
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-function pdf() {
+function ResumeNew() {  // Renamed component to match the export
   const [width, setWidth] = useState(1200);
 
   useEffect(() => {
